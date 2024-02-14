436b774d9319e0d32eea5ca96725ec824865b9f2
Ritik <shrestharitik@gmail.com>
2024-02-14 14:07:29 +0545

    added search

--- src/components/MapBoxDirection.jsx
+++ src/components/MapBoxDirection.jsx
@@ -4,10 +4,71 @@ import useLocation from "../../hooks/useLocation";
 
 const TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 const MapboxDirections = () => {
+  const [search, setSearch] = useState("");
   const [map, setMap] = useState(null);
   const [endCoords, setEndCoords] = useState({});
   const radius = 0.008;
   const start = useLocation();
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${search}.json?proximity=${start}&access_token=${mapboxgl.accessToken}`,
+        { method: "GET" }
+      );
+      const result = await res.json();
+      const data = result.features[0].center;
+
+      if (data) {
+        // coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+        const end = {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: data,
+              },
+            },
+          ],
+        };
+
+        if (map.getLayer("end")) {
+          map.getSource("end").setData(end);
+        } else {
+          map.addLayer({
+            id: "end",
+            type: "circle",
+            source: {
+              type: "geojson",
+              data: {
+                type: "FeatureCollection",
+                features: [
+                  {
+                    type: "Feature",
+                    properties: {},
+                    geometry: {
+                      type: "Point",
+                      coordinates: data,
+                    },
+                  },
+                ],
+              },
+            },
+            paint: {
+              "circle-radius": 10,
+              "circle-color": "#f30",
+            },
+          });
+        }
+        getRoute(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   var createGeoJSONCircle = function (center, radiusInKm, points) {
     if (!points) points = 64;
 
@@ -94,45 +155,50 @@ const MapboxDirections = () => {
       `https://api.mapbox.com/directions/v5/mapbox/walking/${start[0]},${start[1]};${end[0]},${end[1]}?alternatives=true&annotations=duration%2Cdistance&geometries=geojson&language=en&overview=full&steps=true&voice_instructions=true&voice_units=imperial&access_token=${mapboxgl.accessToken}`,
       { method: "GET" }
     );
-    const json = await query.json();
-    const data = json.routes[0];
-    const route = data.geometry.coordinates;
-    const geojson = {
-      type: "Feature",
-      properties: {},
-      geometry: {
-        type: "LineString",
-        coordinates: route,
-      },
-    };
+    if (!query.ok) {
+      const json = await query.json();
+      alert(json.message);
+    } else {
+      const json = await query.json();
+      const data = json.routes[0];
+      const route = data.geometry.coordinates;
+      const geojson = {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "LineString",
+          coordinates: route,
+        },
+      };
 
-    // if the route already exists on the map, we'll reset it using setData
-    if (map.getSource("route")) {
-      map.getSource("route").setData(geojson);
+      // if the route already exists on the map, we'll reset it using setData
+      if (map.getSource("route")) {
+        map.getSource("route").setData(geojson);
+      }
+      // otherwise, we'll make a new request
+      else {
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: {
+            type: "geojson",
+            data: geojson,
+          },
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#3887be",
+            "line-width": 5,
+            "line-opacity": 0.75,
+          },
+        });
+        // get the sidebar and add the instructions
+      }
+      setEndCoords(data);
+      // instructionFunc(data, start);
     }
-    // otherwise, we'll make a new request
-    else {
-      map.addLayer({
-        id: "route",
-        type: "line",
-        source: {
-          type: "geojson",
-          data: geojson,
-        },
-        layout: {
-          "line-join": "round",
-          "line-cap": "round",
-        },
-        paint: {
-          "line-color": "#3887be",
-          "line-width": 5,
-          "line-opacity": 0.75,
-        },
-      });
-    }
-    // get the sidebar and add the instructions
-    setEndCoords(data);
-    // instructionFunc(data, start);
   }
 
   useEffect(() => {
@@ -155,6 +221,7 @@ const MapboxDirections = () => {
       // make a marker for each feature and add it to the map
 
       console.log("start", start);
+
       map.on("click", (event) => {
         coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
         const end = {
@@ -256,7 +323,18 @@ const MapboxDirections = () => {
   return (
     <div className="flex flex-col  ">
       <div id="map" className="w-full h-full overflow-hidden"></div>
-      <div id="instructions" className="p-10"></div>
+      <form onSubmit={handleSearch}>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="absolute top-0 left-0 p-2 z-50"
+          id="search"
+          placeholder="Search location"
+          value={search}
+        />
+      </form>
+
+      <div id="instructions"></div>
     </div>
   );
 };
